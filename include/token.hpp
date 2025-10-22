5ff9c0a63b2d52d68953bd7cb898502f289d60af
WanAdamm <wanadamworkmail@gmail.com>
2025-10-22 18:13:54 +0800

    added some variable to keep track of additional info such as start position, length, line, and column of tokens

--- include/token.hpp
+++ include/token.hpp
@@ -17,5 +17,10 @@ struct Token
 {
     TokenType type;
     std::string value;
+    size_t start_pos; // character index where the token begins
+    size_t length;    // number of characters in the token
+    size_t line;      // line number
+    size_t column;    // column number
+
     Token(TokenType t, std::string v) : type(t), value(std::move(v)) {}
 };
