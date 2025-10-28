7314a9cf03973c51f1c703254faa877554166da6
Amirah <amirah824@student.usm.my>
2025-10-28 18:45:59 +0800

    edited the outputs

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,16 @@
 int main()
 {
     std::string input;
-    std::cout << "===================================\n";
-    std::cout << "    Welcome to the Mini-Compiler\n";
-    std::cout << "===================================\n";
-
+    std::cout << "===================================================\n";
+    std::cout << "           Welcome to the Mini-Compiler\n";
+    std::cout << "===================================================\n";
     // Loop (Easy for Testing)
     while (true)
-    {
-        std::cout << "Enter expression (or 'exit' to quit): ";
+    {   
+        std::cout << " \n";
+        std::cout << "Please enter an expression. (Example: x = 2 + 4;)\n";
+        std::cout << "Type 'exit' to quit the program.\n";
+        std::cout << "Enter here: ";
         std::getline(std::cin, input);
 
         // For Exit command
@@ -31,16 +33,18 @@ int main()
         Parser parser(tokens);
         bool success = parser.parse();
 
-        // C. Summary (Need to print even if fail - Requiremenr)
-        std::cout << "Token Summary\n";
+        // C. Summary (Need to print even if fail - Requirement)
+        std::cout << "---------------------------------------\n";
+        std::cout << "             Token Summary\n";
+        std::cout << "---------------------------------------\n";
         lexer.summarize();
         std::cout << std::endl;
 
         // D. Log and Report Error
-        // Check if Error Persisted (Print Tree or Error - Requirement lepas sumaary)
+        // Check if Error Persisted (Print Tree or Error - Requirement lepas summary)
         if (success && !parser.hasErrors())
         {
-            std::cout << "Valid syntax.\n";
+            std::cout << "---> Valid syntax.\n";
             parser.printSyntaxTree();
         }
         else
